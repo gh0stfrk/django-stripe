3420560250f7b0ae8de60536db3ea5b9e746df46
gh0stfrk <salmansyyd12@gmail.com>
2024-02-29 12:28:43 +0530

    dply: fix allowed_hosts

--- stripedjango/settings.py
+++ stripedjango/settings.py
@@ -31,9 +31,9 @@ SECRET_KEY = env('DJANGO_SECRET_KEY', default='django-insecure-tckvhfo9yeev)1_pm
 # DEBUG = False
 DEBUG = env('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = ['https://django-stripe-sqgd.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['django-stripe-sqgd.onrender.com', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://django-stripe-sqgd.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['django-stripe-sqgd.onrender.com']
 
 # Application definition
 
